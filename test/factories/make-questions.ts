8636bf7f176bd7ad3050b482538fc62cd8bcdeb1
Icaro Ramos <polosnramos@hotmail.com>
2024-06-01 16:22:14 -0300

    chore: Add @faker-js to populate makeQuestion factory for tests

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -4,14 +4,21 @@ import {
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-object/slug";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example Question",
-    slug: Slug.create("example-question"),
-    content: "Example content",
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create("example-question"),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
   return question;
 }
