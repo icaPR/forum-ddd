726621e7cfd5246dc33449e426d2b92723da245d
Icaro Ramos <polosnramos@hotmail.com>
2024-06-07 21:05:26 -0300

    feat: Handle attachments when creating and editing Question entity

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,14 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { Either, right } from "@/core/either";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
 
 interface CreateQuestionUseCaseReq {
   authorId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type CreateQuestionUseCaseRes = Either<
@@ -23,12 +26,22 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentsIds,
   }: CreateQuestionUseCaseReq): Promise<CreateQuestionUseCaseRes> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       title,
       content,
     });
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      });
+    });
+    question.attachments = new QuestionAttachmentList(questionAttachments);
+
     await this.questionsRepository.create(question);
 
     return right({ question });
--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -3,12 +3,17 @@ import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { NotAllowedError } from "./errors/not-allowed-error";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { QuestionAttachmentRepository } from "../repositories/question-attachments-repository";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
 
 interface EditQuestionUseCaseReq {
   questionId: string;
   authorId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type EditQuestionUseCaseRes = Either<
@@ -19,13 +24,17 @@ type EditQuestionUseCaseRes = Either<
 >;
 
 export class EditQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionAttachmentRepository: QuestionAttachmentRepository
+  ) {}
 
   async handle({
     questionId,
     authorId,
     title,
     content,
+    attachmentsIds,
   }: EditQuestionUseCaseReq): Promise<EditQuestionUseCaseRes> {
     const question = await this.questionsRepository.findById(questionId);
 
@@ -36,9 +45,24 @@ export class EditQuestionUseCase {
       return left(new NotAllowedError());
     }
 
+    const currentQuestionAttachments =
+      await this.questionAttachmentRepository.findManyByQuestionId(questionId);
+
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments
+    );
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      });
+    });
+    questionAttachmentList.update(questionAttachments);
+
     question.title = title;
     question.content = content;
-
+    question.attachments = questionAttachmentList;
     await this.questionsRepository.save(question);
 
     return right({ question });
