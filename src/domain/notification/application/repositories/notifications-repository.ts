719d4ffc44a55e7128d7f116166a7219605c7c54
Icaro Ramos <polosnramos@hotmail.com>
2024-06-08 22:26:24 -0300

    feat: Create use case to read Notification

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from "../../enterprise/entities/notfication";
 
 export interface NotificationsRepository {
   create(notification: Notification): Promise<void>;
+  findById(id: string): Promise<Notification | null>;
+  save(notification: Notification): Promise<void>;
 }
